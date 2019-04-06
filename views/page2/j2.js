9f018093b8c37e2bc6767afa589b0130271a9bf7
Carnaux <danielfeenandes@gmail.com>
2019-04-06 19:45:01 -0300

    Update

--- views/page2/j2.js
+++ views/page2/j2.js
@@ -6,16 +6,22 @@ renderer2.setSize( 500, 400 );
 let renderDiv2 = document.getElementById("renderDiv2");
 renderDiv2.appendChild( renderer2.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: new THREE.Color('rgb(219,219,219)'), transparent: true, opacity: 0.6 } );
-var cube = new THREE.Mesh( geometry, material );
-scene2.add( cube );
-var edges = new THREE.EdgesGeometry( geometry );
-var line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: new THREE.Color('rgb(0,0,0)') } ) );
-line.material.linewidth = 5;
-cube.add( line );
+// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// var material = new THREE.MeshBasicMaterial( { color: new THREE.Color('rgb(219,219,219)'), transparent: true, opacity: 0.6 } );
+// var cube = new THREE.Mesh( geometry, material );
+// scene2.add( cube );
+// var edges = new THREE.EdgesGeometry( geometry );
+// var line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: new THREE.Color('rgb(0,0,0)') } ) );
+// line.material.linewidth = 5;
+// cube.add( line );
 
-camera2.position.z = 5;
+camera2.position.z = 8;
+
+var light = new THREE.AmbientLight( new THREE.Color('rgb(255,255,255)'), 0.2 ); // soft white light
+scene2.add( light );
+
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+scene2.add( directionalLight );
 
 var controls2 = new THREE.OrbitControls( camera2 );
 controls2.enableZoom = false;
@@ -64,6 +70,38 @@ scene2.add(mesh);
 
 renderDiv2.addEventListener('mouseover', mouseOver);
 renderDiv2.addEventListener('mouseleave', mouseLeave);
+
+var mtlLoader = new THREE.MTLLoader();
+mtlLoader.setPath( 'assets/models/' );
+var url = "house1.mtl";
+mtlLoader.load( url, function( materials ) {
+
+    materials.preload();
+
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials( materials );
+    objLoader.setPath( 'assets/models/' );
+    objLoader.load( 'house1.obj', function ( object ) {
+
+        
+       
+        object.traverse( function ( child ) {
+
+            if ( child instanceof THREE.Mesh ) {
+                child.material.color = new THREE.Color('rgb(219,219,219)'); 
+                child.material.transparent = true;
+                child.material.opacity = 0.3; 
+                scene2.add(child);
+                var edges = new THREE.EdgesGeometry( child.geometry );
+                var line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: new THREE.Color('rgb(0,0,0)') } ) );
+                line.material.linewidth = 5;
+                child.add( line );
+            }
+        } );
+
+    });
+
+});
 
 var animate2 = function () {
     requestAnimationFrame( animate2 );
